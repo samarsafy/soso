51a37f686623093e23f23243c8c7b6088ebe39d9
Unknown <samarsafy@yahoo.de>
2018-01-11 15:55:41 +0100

    Changes in checkname and Email

--- frontend_test.js
+++ frontend_test.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
+ 
   document.getElementById('test_input').addEventListener("submit", function(e){
     e.preventDefault();
-    console.log( checkUsername(e.target[0].value) );
+
+    
+    console.log( checkUsername(e.target.elements[0].value));
+    if (checkUsername(e.target[0].value)){
+    	e.target[0].classList
+    }
+    console.log (checkEmail(e.target.elements[1].value));
   });
 });
--- tests.js
+++ tests.js
@@ -1,5 +1,12 @@
 const checkUsername = (user) => {
   return /^[a-zA-Z]+$/.test(user)
 }
+const checkEmail=(email)=> {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    
+}
 
-console.log("username  just letters: ",  checkUsername("Tommy"))
+
+
+console.log("Emailadress is correct", checkEmail("samarsafy@yahoo.de"));
+console.log("username  just letters: ",  checkUsername("Tommy"));
